867a86231f7ac268ff379bd777b79289dc909949
kevinrad09 <kevinrademacher@MacBook-Pro.omaha.org>
2016-09-12 19:23:33 -0500

    fixed website

---
+++ kevinrademacherassessment/orange.js
@@ -0,0 +1,20 @@
+            /*$("#slide").hover(function(){
+                $("#slide").slideUp("slow");
+            });
+            
+            $("#slide").hover(function(){
+                $("#slide").slideDown("slow");
+            });
+            */
+            
+            $("#slide").click(function(){
+
+                $("#popup").fadeIn(1000);
+
+            });
+            
+            $("#close").click(function(){
+
+                $("#popup").fadeOut(1000);
+
+            });
